5244463f1aafdea78a8c7378fe2ab93c5a51ae1e
Yogesh Panjwani <panjwaniyogesh95@gmail.com>
2021-07-04 23:13:07 +0530

    ball bouncing logic added

--- main.py
+++ main.py
@@ -27,5 +27,10 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    #Detect collission with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #needs to bounce
+        ball.bounce()
+
 
 screen.exitonclick()
